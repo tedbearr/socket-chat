ce82cda8dbd056a09145830fa0be220e9ff71dae
tedbearr <teddystwn48@gmail.com>
2022-09-02 10:37:25 +0700

    first

---
+++ server/index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const socket = require("socket.io");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+app.use(express.json());
+
+const server = app.listen("3002", () => {
+  console.log("Running on port 3002");
+});
+
+io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "X-Requested-With",
+      "Accept",
+      "Origin",
+      "Access-Control-Allow-Origin",
+      "Access-Control-Allow-Headers",
+      "Access-Control-Allow-Methods",
+      "Access-Control-Allow-Credentials",
+    ],
+    withCredentials: true,
+  },
+});
+
+io.on("connection", (socket) => {
+  console.log(socket.id);
+
+  socket.on("join_room", (data) => {
+    socket.join(data);
+    console.log('User joined room: ' + data)
+  });
+
+  socket.on("disconnected", () => {
+    console.log("User disconnected");
+  });
+});
